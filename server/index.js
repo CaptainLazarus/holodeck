5e877c4a85f979798d9e8bf4fb7f94c588dc10d3
CaptainLazarus <adityagudimetla@gmail.com>
2024-03-02 06:57:10 +0530

    "Configuration complete"

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 // In server/index.js
-import express from 'express'
-import path from 'path'
+import express from 'express';
+import path from 'path';
 
-const app = express()
-const PORT = process.env.PORT ?? 3000
+const app = express();
+const PORT = process.env.PORT ?? 3000;
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join('public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
-})
+  res.sendFile(path.resolve('public', 'index.html'));
+});
 
-app.listen(PORT, () => { console.log(`Server running on http://localhost:${PORT}`) })
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
